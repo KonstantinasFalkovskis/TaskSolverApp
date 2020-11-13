28359c6a9d35e5ca1b43303deb6a436ac5717f4b
FalcoConstantine <integro@inbox.lt>
2020-11-13 23:03:32 +0200

    Add application logic

--- src/main/java/oop/Autochecked.java
+++ src/main/java/oop/Autochecked.java
@@ -1,20 +1,7 @@
 package oop;
 
-/**
- * Autochecked
- *
- * Используются, чтобы отметить задания, которые проверяются автоматически.
- */
 public interface Autochecked {
 
-    /**
-     * Методы
-     * ✷ default метод Выполнить задание
-     * Без аргументов
-     * Тип возвращаемого значения
-     * ➜ void
-     * Печатает сообщение о том, что задание выполнено
-     */
     default boolean taskAutochecking(int number) {
         boolean isAutocheck = false;
         if (number % 2 == 0) {
@@ -22,7 +9,6 @@ public interface Autochecked {
         } else {
             isAutocheck = false;
         }
-//        System.out.println("The task is completed!!!");
         return isAutocheck;
     }
 
--- src/main/java/oop/DragAndDrop.java
+++ src/main/java/oop/DragAndDrop.java
@@ -1,24 +1,11 @@
 package oop;
 
-/**
- * DragAndDrop (Наследник Task, реализует интерфейс Autochecked)
- */
-
 public class DragAndDrop extends Task implements Autochecked {
 
-    /**
-     * Поля
-     * ✷ Массив с вариантами
-     *      ➜ Тип поля двумерный массив строк
-     */
     private String[][] possibleAnswers;
 
-    /**
-     * Конструктор по умолчанию вызывает конструктор родителя и массиву присваивает ссылку
-     * на новый двумерный массив, заполненный значениями по вашему усмотрению
-     */
     public DragAndDrop(int number, String taskText) {
         super(number, taskText);
-        this.possibleAnswers = new String[][]{{"1","One"},{"2","Two"},{"3","Tree"}};
+        this.possibleAnswers = new String[][]{{"1", "One"}, {"2", "Two"}, {"3", "Tree"}};
     }
 }
